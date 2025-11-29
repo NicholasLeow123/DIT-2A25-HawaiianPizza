f516776bb6826c6e771b0d5931f16ce5f51ee601
NicholasLeow123 <NICHOLASL.24@ichat.sp.edu.sg>
2025-11-29 19:55:26 +0800

    added caching for first time audio is played for faster loading of audio file next time

--- src/public/soundButton.js
+++ src/public/soundButton.js
@@ -12,5 +12,18 @@ const AudioPlayer = (() => {
    * @param {number} volume - Volume level (0.0 to 1.0), default 0.5
    * @returns {Promise} - Resolves when sound plays
    */
+  const play = (audioPath, volume = 0.5) => {
+    let audio;
 
+    //use cached audio if available
+    if (audioCache.has(audioPath)) {
+        audio = audioCache.get(audioPath);
+    } else {
+        //create new audio element
+        audio = new Audio(audioPath);
+        audio.volume = volume;
+        audio.preload = 'auto';
+        audioCache.set(audioPath, audio);
+    }
+  }
 });
