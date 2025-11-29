c6122f39156e93e4fabe361afdd5b7057f0a0aac
NicholasLeow123 <NICHOLASL.24@ichat.sp.edu.sg>
2025-11-29 19:43:25 +0800

    added documentation for audioplayer function

--- src/public/soundButton.js
+++ src/public/soundButton.js
@@ -5,4 +5,12 @@
 const AudioPlayer = (() => {
   // Private variables
   const audioCache = new Map();
+
+  /**
+   * Play an audio file
+   * @param {string} audioPath - Path to the audio file
+   * @param {number} volume - Volume level (0.0 to 1.0), default 0.5
+   * @returns {Promise} - Resolves when sound plays
+   */
+
 });
