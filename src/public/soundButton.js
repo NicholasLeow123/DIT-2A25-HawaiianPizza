2847f6069b1d6a7ade2141c35559eeadb787e98f
NicholasLeow123 <NICHOLASL.24@ichat.sp.edu.sg>
2025-11-29 19:36:44 +0800

    added constant audioplayer

---
+++ src/public/soundButton.js
@@ -0,0 +1,8 @@
+/*  Sound button module
+    can be attached to any button with data-audio attribute
+*/
+
+const AudioPlayer = (() => {
+  // Private variables
+  const audioCache = new Map();
+});
